0af0aef762d9fe58bc8ed1345856bd621439e06e
marouanbouchettoy <uanemaro216@gmail.com>
2024-12-28 05:05:03 +0100

    Refactored some code.

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from random import randint, choice, random
 from datetime import datetime, timedelta
 
 # Définir les dates de début et de fin
-start_date = datetime(2023, 3, 31)
-end_date = datetime(2024, 12, 30)
+start_date = datetime(2024, 12, 28)
+end_date = datetime(2025, 5, 9)
 
 # Liste de messages de commit aléatoires
 commit_messages = [
